e86dabcd2c7db5bdc22926777f4988aabb314ec9
Filippo Bersani <filobers@yahoo.it>
2019-03-14 08:37:11 +0100

    Add files via upload

---
+++ sweep.rb
@@ -0,0 +1,52 @@
+=begin
+ *** ICMP ping sweep of a net segment as xxx.xxx.xxx. ***
+ *** Required gems: net-ping, win32-security (on windows only) ***
+ *** v. 1.0 Zinzloun ***
+=end
+
+#import gem
+require 'net/ping'
+if ARGV.empty?
+  puts "Usage: sweep.rb <net segment e.g. 192.168.1.> [-v print number of alive hosts] [-f redirect output to a file named sweep.txt]"
+  exit 1
+end
+
+puts "Sweep started, please wait...\n"
+#get the net segment
+net_seg = ARGV[0]
+#default (optional) is 1 second, decrease it to speed up
+timeout = 0.15
+ip_count = 0
+output_s = ""
+
+#subnet range loop
+(1..254).each do |ip|
+ #ping a host
+ req = Net::Ping::ICMP.new(net_seg + ip.to_s,nil,timeout)
+ #check is alive
+ if req.ping
+	output_s += net_seg + ip.to_s + "\n"
+	puts net_seg + ip.to_s
+	ip_count += 1
+ end
+end
+
+#verbose
+if ARGV.include?("-v")
+ puts "Alive hosts: " + ip_count.to_s
+end
+
+#write output to the file
+if ARGV.include?("-f")
+ begin
+  file = File.open("./sweep.txt", "w")
+  file.write(output_s)
+  puts "Scan result saved in file sweep.txt"
+ rescue IOError => e
+  #some error occur, dir not writable etc...
+  puts "An error occured creating the sweep output file. " + e.to_s
+ ensure
+  file.close unless file.nil?
+ end
+end
+
