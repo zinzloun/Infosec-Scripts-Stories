f1f40bf67515d3a844f529900e3f4c0872fa081d
Filippo Bersani <filobers@yahoo.it>
2019-11-26 13:19:08 +0100

    Add files via upload

---
+++ cewl2ascii.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script clean up a wordlist created with cewel (required)
+#  you must provide a valid url
+
+if [[ $# -eq 0 ]] ; then
+    echo "You must provide a valid URL. Usage $0 <URL>"
+    exit 1
+fi
+
+echo "Generating the wordlist from $1..."
+cewl "$1" -d 1 -w tmp_wl.txt
+# clean up the result for wikipedia derived wl 1
+echo "done. Clean up the result..."
+cat tmp_wl.txt | grep "\w\{7,\}" | grep -v "^wg" | head -n -50 > tmp_wl1.txt
+# get only character class
+grep -P '^[[:ascii:]]+$' tmp_wl1.txt > aniceWL.txt
+#clean up files
+rm tmp_wl*.txt
+echo "The wordlist has been created: aniceWL.txt"
