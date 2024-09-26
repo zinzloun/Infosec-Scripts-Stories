8e26b2d8ae42ff03b40558869d862ada60eb43be
Filippo Bersani <filobers@protonmail.com>
2024-09-26 12:10:42 +0200

    Update ass_rem_cert.ps1

--- ass_rem_cert.ps1
+++ ass_rem_cert.ps1
@@ -2,7 +2,7 @@
 $host_ = "localhost"
 $port = 443
 
-# Set TLS version
+# Set TLS version, on Win11 you can use [SystemDefault;Ssl3;Tls;Tls11;Tls12;Tls13]
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls13
 $TCPClient = New-Object -TypeName System.Net.Sockets.TCPClient
 $TcpSocket = New-Object Net.Sockets.TcpClient($host_,$port)
